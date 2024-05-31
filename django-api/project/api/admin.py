65ef63b6b62f8630577f15936b19128246a83940
Tiago Oliveira <contacto.tiagooliveira@gmail.com>
2024-05-31 23:46:25 +0100

    Crud Local to api, App improvements and some minor fixes

--- django-api/project/api/admin.py
+++ django-api/project/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CustomUser, Travel
+from .models import CustomUser, Travel, Spot
 
 admin.site.register(CustomUser)
 admin.site.register(Travel)
+admin.site.register(Spot)
 
